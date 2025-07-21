a5254051903f568a2bae0028d7bd32361b3892c6
shubhdevv <shubhamdhoni250804@gmail.com>
2025-07-21 10:24:51 +0530

    subsrciption model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -211,6 +211,8 @@ const refreshAccessToken = asyncHandler(async(req,res) => {
 })
 
 
+
+
 export {
   registerUser,
   loginUser,
---
+++ src/models/subscription.models.js
@@ -0,0 +1,19 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // subscriber
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // channel
+        ref: "User"
+    },
+},
+{timestamps: true})
+
+
+
+
+
+export const subscription = mongoose.model("Subscription", subscriptionSchema)
